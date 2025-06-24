44b7d3851c4f12bce6ce6f805732cb6974802260
Kaiwal <38000481+KaiwalPanchal@users.noreply.github.com>
2025-06-24 13:45:04 +0530

    Update __init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -2,8 +2,3 @@ from .main import query_builder_agent
 from .main import query_answerer_agent
 from .main import ROOT_AGENT
 from .main import query_planner_agent
-
-from .test import query_builder_agent
-from .test import query_answerer_agent
-from .test import ROOT_AGENT
-from .test import query_planner_agent
